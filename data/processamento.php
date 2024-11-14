6abe56425ccc2546d32604ff46a7bef8d7ed0a99
felipeshicasho <felipetoleds@gmail.com>
2024-11-14 08:39:50 -0300

    Organizando a estrutura

--- data/processamento.php
+++ data/processamento.php
@@ -2,84 +2,79 @@
 session_start();
 require "funcoesBD.php";
 
-// DELETAR EMPRESA
-if (isset($_GET['acao']) && $_GET['acao'] === 'deletar' && isset($_GET['id'])) {
-    $id = $_GET['id'];
-    deletarEmpresa($id);
-    header('Location: ../client/view/removerEmpresa.php');
-    exit();
-} else if (isset($_GET['acao']) && $_GET['acao'] === 'deletarSetor' && isset($_GET['id'])) {
-    $id = $_GET['id'];
-    deletarSetor($id);
-    header('Location: ../client/view/removerSetor.php');
-    exit();
-}
-else if (isset($_GET['acao']) && $_GET['acao'] === 'deletarEmpresaSetor' && isset($_GET['empresa_id']) && isset($_GET['setor_id'])) {
-    $empresa_id = $_GET['empresa_id'];
-    $setor_id = $_GET['setor_id'];
-    deletarEmpresaSetor($empresa_id, $setor_id);
-    header('Location: ../client/view/removerEmpresaSetor.php');
-    exit();
+// FUNÇÕES DELETAR
+if (isset($_GET['acao']) && $_GET['acao'] === 'deletar') {
+    // Deletar Empresa
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        deletarEmpresa($id);
+        header('Location: ../client/view/removerEmpresa.php');
+        exit();
+    }
+} else if (isset($_GET['acao']) && $_GET['acao'] === 'deletarSetor') {
+    // Deletar Setor
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        deletarSetor($id);
+        header('Location: ../client/view/removerSetor.php');
+        exit();
+    }
+} else if (isset($_GET['acao']) && $_GET['acao'] === 'deletarEmpresaSetor') {
+    // Deletar Empresa-Setor
+    if (isset($_GET['empresa_id']) && isset($_GET['setor_id'])) {
+        $empresa_id = $_GET['empresa_id'];
+        $setor_id = $_GET['setor_id'];
+        deletarEmpresaSetor($empresa_id, $setor_id);
+        header('Location: ../client/view/removerEmpresaSetor.php');
+        exit();
+    }
 }
 
-// ADICIONAR EMPRESA
-if (
-    !empty($_POST['inputRazaoSocial']) && !empty($_POST['inputNomeFantasia']) &&
-    !empty($_POST['inputCnpj'])
-) {
+// FUNÇÕES INSERIR
+if (!empty($_POST['inputRazaoSocial']) && !empty($_POST['inputNomeFantasia']) && !empty($_POST['inputCnpj'])) {
+    // Inserir Empresa
     $razao_social = $_POST['inputRazaoSocial'];
     $nome_fantasia = $_POST['inputNomeFantasia'];
     $cnpj = $_POST['inputCnpj'];
 
-
     inserirEmpresa($razao_social, $nome_fantasia, $cnpj);
-    header('Location:../client/view/adicionarEmpresa.php');
-    die();
-}
-// INSERIR SETOR
-else if (!empty($_POST['inputDescricao'])) {
+    header('Location: ../client/view/adicionarEmpresa.php');
+    exit();
+} else if (!empty($_POST['inputDescricao'])) {
+    // Inserir Setor
     $descricao = $_POST['inputDescricao'];
     inserirSetor($descricao);
     header('Location: ../client/view/visualizarSetor.php');
-    die();
-}
-
-// MODIFICAR EMPRESA
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['acao']) && $_POST['acao'] === 'modificar') {
-    $id = $_POST['id'];
-
-
-    $razao_social = $_POST['razao_social'];
-    $nome_fantasia = $_POST['nome_fantasia'];
-    $cnpj = $_POST['cnpj'];
-
-    modificarEmpresa($id, $razao_social, $nome_fantasia, $cnpj);
-    header('Location: ../client/view/visualizarEmpresa.php');
     exit();
-}
-// MODIFICAR SETOR
-else if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['acao']) && $_POST['acao'] === 'modificarSetor') {
-    $id = $_POST['id'];
-
-
-    $descricao = $_POST['descricao'];
-
-    modificarSetor($id, $descricao);
-    header('Location: ../client/view/visualizarSetor.php');
-    exit();
-}
-
-
-//  EMPRESA ---- SETOR
-
-// ADICIONAR EMPRESA
-if (
-    !empty($_POST['inputOptionEmpresa']) && !empty($_POST['inputOptionSetor'])
-) {
+} else if (!empty($_POST['inputOptionEmpresa']) && !empty($_POST['inputOptionSetor'])) {
+    // Inserir Empresa-Setor
     $empresa_id = $_POST['inputOptionEmpresa'];
     $setor_id = $_POST['inputOptionSetor'];
 
     inserirEmpresaSetor($empresa_id, $setor_id);
     header('Location: ../client/view/visualizarEmpresaSetor.php');
-    die();
+    exit();
+}
+
+// FUNÇÕES MODIFICAR
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['acao'])) {
+    if ($_POST['acao'] === 'modificar') {
+        // Modificar Empresa
+        $id = $_POST['id'];
+        $razao_social = $_POST['razao_social'];
+        $nome_fantasia = $_POST['nome_fantasia'];
+        $cnpj = $_POST['cnpj'];
+
+        modificarEmpresa($id, $razao_social, $nome_fantasia, $cnpj);
+        header('Location: ../client/view/visualizarEmpresa.php');
+        exit();
+    } else if ($_POST['acao'] === 'modificarSetor') {
+        // Modificar Setor
+        $id = $_POST['id'];
+        $descricao = $_POST['descricao'];
+
+        modificarSetor($id, $descricao);
+        header('Location: ../client/view/visualizarSetor.php');
+        exit();
+    }
 }
